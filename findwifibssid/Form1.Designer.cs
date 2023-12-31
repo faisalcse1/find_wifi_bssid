f43ad3715a05b552d4bb40cab3e638e3ad82a23b
Md Faisal <faisalcse1@gmail.com>
2023-12-31 14:22:32 +0600

    method added for find bssid

--- findwifibssid/Form1.Designer.cs
+++ findwifibssid/Form1.Designer.cs
@@ -31,8 +31,10 @@
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.wifiNameTextBox = new System.Windows.Forms.TextBox();
             this.findButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.bssidTextbox = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -49,13 +51,15 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.findButton);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.bssidTextbox);
+            this.groupBox1.Controls.Add(this.wifiNameTextBox);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(21, 59);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(400, 212);
+            this.groupBox1.Size = new System.Drawing.Size(400, 195);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "BSSID";
@@ -69,27 +73,44 @@
             this.label2.TabIndex = 0;
             this.label2.Text = "Wifi Name";
             // 
-            // textBox1
+            // wifiNameTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(152, 43);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(221, 29);
-            this.textBox1.TabIndex = 1;
+            this.wifiNameTextBox.Location = new System.Drawing.Point(152, 43);
+            this.wifiNameTextBox.Name = "wifiNameTextBox";
+            this.wifiNameTextBox.Size = new System.Drawing.Size(221, 29);
+            this.wifiNameTextBox.TabIndex = 1;
             // 
             // findButton
             // 
-            this.findButton.Location = new System.Drawing.Point(294, 78);
+            this.findButton.Location = new System.Drawing.Point(294, 83);
             this.findButton.Name = "findButton";
             this.findButton.Size = new System.Drawing.Size(79, 32);
             this.findButton.TabIndex = 2;
             this.findButton.Text = "Find";
             this.findButton.UseVisualStyleBackColor = true;
+            this.findButton.Click += new System.EventHandler(this.findButton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 24);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "BSSID";
+            // 
+            // bssidTextbox
+            // 
+            this.bssidTextbox.Location = new System.Drawing.Point(152, 127);
+            this.bssidTextbox.Name = "bssidTextbox";
+            this.bssidTextbox.Size = new System.Drawing.Size(221, 29);
+            this.bssidTextbox.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(443, 283);
+            this.ClientSize = new System.Drawing.Size(443, 271);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
@@ -107,9 +128,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox wifiNameTextBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button findButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox bssidTextbox;
     }
 }
 
--- findwifibssid/Form1.cs
+++ findwifibssid/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,51 @@ namespace findwifibssid
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void findButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(wifiNameTextBox.Text.Trim()))
+                {
+                    MessageBox.Show("Please enter wifi name.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                string wifiName=wifiNameTextBox.Text;                
+                string bssId=FindBssid(wifiName);
+                if (string.IsNullOrEmpty(bssId))
+                {
+                    MessageBox.Show("BSSID not found.", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    bssidTextbox.Clear();
+                }
+                bssidTextbox.Text = bssId;
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        static string FindBssid(string ssid)
+        {
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (nic.NetworkInterfaceType == NetworkInterfaceType.Wireless80211 && nic.Description.ToLower().Contains("wireless"))
+                {
+                    foreach (var unicastAddress in nic.GetIPProperties().UnicastAddresses)
+                    {
+                        if (unicastAddress.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {                            
+                            if (nic.GetPhysicalAddress() != null && nic.GetPhysicalAddress().ToString() != "")
+                            {
+                                return nic.GetPhysicalAddress().ToString();
+                            }
+                        }
+                    }
+                }
+            }
+
+            return null;
         }
     }
 }
